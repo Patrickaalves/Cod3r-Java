2c39fa2e5d690123ad5f474a40e12576deeb4536
Patrickaalves <patrickalvessc@gmail.com>
2023-03-12 13:34:39 -0300

    Alguns fundamentos gerais e um exercicio

---
+++ exericios/src/fundamentos/AreaCircunferenciaDesafio.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class AreaCircunferenciaDesafio {
+    public static void main(String[] args) {
+        double valorF = 86;
+        double vlF = valorF - 32;
+        double resultado = vlF * (5/9.0);
+
+        System.out.println("O valor de " + valorF +" Fah em graus Celsius = "+resultado);
+    }
+}
