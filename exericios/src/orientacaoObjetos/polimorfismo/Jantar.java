19506154a9663b4696ea42ad5bc860fe4e4e06aa
Patrickaalves <patrickalvessc@gmail.com>
2023-03-17 13:55:57 -0300

    Algumas modificacoes

--- exericios/src/orientacaoObjetos/polimorfismo/Jantar.java
+++ exericios/src/orientacaoObjetos/polimorfismo/Jantar.java
@@ -7,10 +7,19 @@ public class Jantar {
         System.out.println();
 
         Arroz ingrediente1 = new Arroz(0.300);
-        Feijao ingrediente2 = new Feijao(0.200);
-        Sorvete sorbemesa = new Sorvete(0.500);
+        Feijao ingrediente2 = new Feijao(0.700);
+        Sorvete sorbemesa = new Sorvete(0.5);
+        Sorvete sobre2 = new Sorvete(7);
+
 
         p1.comer(ingrediente1);
+
+        p1.comer(ingrediente2);
+
+        p1.comer(sobre2);
+
+        p1.comer(sorbemesa);
+
 
         System.out.println("O peso da pessoa: "+p1.getPeso());
     }
--- exericios/src/orientacaoObjetos/polimorfismo/Sorvete.java
+++ exericios/src/orientacaoObjetos/polimorfismo/Sorvete.java
@@ -5,4 +5,23 @@ public class Sorvete extends Comida{
         super(peso);
     }
 
+    boolean validaPesoSorvete(double peso){
+        if (peso > 2){
+            System.out.println("O peso esta muito grande");
+            return false;
+        }else {
+            return true;
+        }
+    }
+    @Override
+    public void setPeso(double peso) {
+
+        if (validaPesoSorvete(peso)){
+            super.setPeso(peso);
+        }else {
+            super.setPeso(this.getPeso() - peso);
+        }
+
+
+    }
 }
